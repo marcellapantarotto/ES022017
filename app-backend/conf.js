6240c233a3d2a6bd29d00d306e78427414e50cc9
Flavio Silva <flavio.asilva@gmail.com>
2017-10-18 01:27:41 -0200

    updating

--- app-backend/conf.js
+++ app-backend/conf.js
@@ -1,10 +1,13 @@
-MONGO_URL = undefined;
-BACKEND_API_URL = undefined;
-BACKEND_API_PORT = undefined;
-BACKEND_API_DNS = undefined;
+var MONGO_URL = undefined;
+var BACKEND_API_URL = undefined;
+var BACKEND_API_PORT = undefined;
+var BACKEND_API_DNS = undefined;
 
 if(process.env.NODE_ENV == "production"){
 
 }else{
-
+  MONGO_URL = undefined;
+  BACKEND_API_URL = undefined;
+  BACKEND_API_PORT = undefined;
+  BACKEND_API_DNS = undefined;
 }
