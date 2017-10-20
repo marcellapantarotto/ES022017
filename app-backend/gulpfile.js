5ed7b09e43997aeb52e6786e6af15097d903d8a8
Flavio Silva <flavio.asilva@gmail.com>
2017-10-20 01:36:24 -0200

    More gulp tasks and production runtime configured by env.

--- app-backend/gulpfile.js
+++ app-backend/gulpfile.js
@@ -1,5 +1,50 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const MongoInMemory = require('mongo-in-memory');
+const spawn = require('child_process').spawn;
+var node;
+var mongoServerInstance;
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('startMongoInMemory', ['stopMongoInMemory'], function() {
+  mongoServerInstance = new MongoInMemory(); //DEFAULT PORT is 27017
+  mongoServerInstance.start(function(error, config){
+      if(error){
+          console.error(error);
+      }else{
+          var mongouri = mongoServerInstance.getMongouri("headshot");
+          console.log('Started MongoInMemory');
+          console.log('Monggo URI: ' + mongouri);
+      }
+  });
 });
+
+gulp.task('stopMongoInMemory', function(){
+  if(mongoServerInstance){
+    mongoServerInstance.stop(function(error){
+      if(error){
+        console.error(error);
+      }
+      console.log('Stoped MongoInMemory');
+    });
+  }
+});
+
+gulp.task('backend', function(){
+  if(node) node.kill();
+  node = spawn('node', ['index.js'], {stdio: 'inherit'})
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+});
+
+
+gulp.task('watchBackend', ['backend'], function(){
+  gulp.watch(['./index.js', './**/*.js'], ['backend', 'startMongoInMemory']);
+});
+
+process.on('exit', function() {
+    if (node) node.kill()
+});
+
+gulp.task('default', ['startMongoInMemory', 'watchBackend']);
--- app-backend/index.js
+++ app-backend/index.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
+var MONGO_URL,
+    BACKEND_API_PORT,
+    BACKEND_API_DNS,
+    BACKEND_API_URL,
+    BACKEND_API_IP_BIND;
+
+if(process.env.NODE_ENV == "production"){
+  MONGO_URL = process.env.MONGO_URL;
+  BACKEND_API_PORT = process.env.BACKEND_API_IP_BIND;
+  BACKEND_API_DNS = process.env.BACKEND_API_DNS;
+  BACKEND_API_URL = process.env.BACKEND_API_URL;
+  BACKEND_API_IP_BIND = process.env.BACKEND_API_IP_BIND;
+}else{
+  MONGO_URL = 'mongodb://127.0.0.1:27017/headshot';
+  BACKEND_API_PORT = 3000;
+  BACKEND_API_DNS = 'localhost';
+  BACKEND_API_URL = 'http://' + BACKEND_API_DNS + ':' + BACKEND_API_PORT;
+  BACKEND_API_IP_BIND = '127.0.0.1'
+}
+
 var express = require('express');
 var app = express();
 
@@ -10,6 +30,6 @@ app.get('/', function (req, res) {
   res.send(JSON.stringify(response));
 });
 
-app.listen(3000, '127.0.0.1', function () {
-  console.log('Example app listening on port 3000!');
+app.listen(BACKEND_API_PORT, BACKEND_API_IP_BIND, function () {
+  console.log('Backend listening on ' + BACKEND_API_URL);
 });
