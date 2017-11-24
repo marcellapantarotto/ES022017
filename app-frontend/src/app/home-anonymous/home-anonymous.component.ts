1696a6d0918a7f88fdffa6bbab737264644c72ac
Marcella Pantarotto <marcellapantarotto@gmail.com>
2017-11-24 18:15:44 -0200

    add css files to components

--- app-frontend/src/app/home-anonymous/home-anonymous.component.ts
+++ app-frontend/src/app/home-anonymous/home-anonymous.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-anonymous',
-  templateUrl: './home-anonymous.component.html'
+  templateUrl: './home-anonymous.component.html',
+  styleUrls: ['./home-anonymous.component.css']
 })
 export class HomeAnonymousComponent implements OnInit {
   title = 'HeadShot';
